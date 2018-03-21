6a7dda780a959fddf1d2541f440dc6b79422b275
sebastien vallet <sebastien.vallet89@outlook.com>
2018-03-21 17:00:00 +0100

    debut chargement clients

---
+++ ReclamationsClient/ClsClient.vb
@@ -0,0 +1,20 @@
+﻿Public Class ClsClient
+
+    Property _Code As String
+    Property _Nom As String
+    Property _Contact As String
+    Property _Tel As String
+    Property _Mail As String
+
+    Public Sub New(Code As String, Nom As String, Contact As String, Tel As String, Mail As String)
+
+        _Code = Code
+        _Nom = Nom
+        _Contact = Contact
+        _Tel = Tel
+        _Mail = Mail
+
+    End Sub
+
+
+End Class
---
+++ ReclamationsClient/ClsSQLReclamationClient.vb
@@ -0,0 +1,22 @@
+﻿Public Class ClsSQLReclamationClient
+
+    'Property _lesClients As New List(Of ClsClient)
+
+    Public Sub New()
+        'readLesClients()
+    End Sub
+
+    Public Function readLesClientsByCode(Code As String) As List(Of ClsClient)
+        Dim lesClients As New List(Of ClsClient)
+        Using s_FbMyReader As New ClassConnection.ClsOdbcConnection("SELECT CLCTABRE, CLCTNOM, CLCTCONTA1, CLCTTELEP1, CLCTEMAIL FROM [pmi].[dbo].[CLIENT] WHERE CLCTABRE LIKE '" & Code & "' ORDER BY CLCTABRE", ClassConnection.ClsChaineConnection.ChaineConnection.CEGID_ReadOnly)
+            With s_FbMyReader
+                While .OdbcReader.Read
+                    Dim leClient As New ClsClient(.OdbcReader("CLCTABRE"), .OdbcReader("CLCTNOM"), .OdbcReader("CLCTCONTA1"), .OdbcReader("CLCTTELEP1"), .OdbcReader("CLCTEMAIL"))
+                    lesClients.Add(leClient)
+                End While
+            End With
+        End Using
+        Return lesClients
+    End Function
+
+End Class
